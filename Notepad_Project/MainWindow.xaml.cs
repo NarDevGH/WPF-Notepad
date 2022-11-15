aa88dea45c641d77ad2cec046cb431c11c684a5d
NarMaroth <bautista.mateo@outlook.com>
2022-11-15 00:39:32 -0300

    first commit

---
+++ Notepad_Project/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Notepad_Project.controls;
+namespace Notepad_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Window_Btn_Clicked(object sender, RoutedEventArgs e) 
+        {
+            File_Options file_Options = new File_Options();
+            AddChild((UserControl)file_Options);
+            file_Options.PointFromScreen(new Point(0,0));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Notepad_Project/controls/File_Options.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad_Project.controls
+{
+    /// <summary>
+    /// Interaction logic for File_Options.xaml
+    /// </summary>
+    public partial class File_Options : UserControl
+    {
+        public File_Options()
+        {
+            InitializeComponent();
+        }
+    }
+}
